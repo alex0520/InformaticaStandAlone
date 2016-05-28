f37b252ed2cf25ffd2344c7324cf44878ef340da
aceherdz <aceherdz@gmail.com>
2016-05-28 15:07:15 -0500

    ajuste shell

--- MaquinaServicio.sh
+++ MaquinaServicio.sh
@@ -10,7 +10,7 @@ echo seteamos el volume a 110%.....
 amixer cset numid=1 110%
 echo validamos que no exista una instancia ya activa
 cant_procesos=$(ps -fea | awk '/queuemanager.js/{print $0}' | wc)
-if[$cant_procesos -eq 1];
+if [ $cant_procesos -eq 1 ]
 then
    node $pathQueueManager/queuemanager.js
 fi
