8f3f6079fabbd2f881e27aa70e9639a95b3919e1
alex0520 <alexander.lozanof@gmail.com>
2016-05-30 13:17:34 -0500

    Se modifica conexión con el gestor de colas

--- queuemanager.js
+++ queuemanager.js
@@ -3,17 +3,41 @@ var dispensador = require('./dispensarDulce');
 var log4c = require('./log4Candy');
 //var musica = require('./play');
 
-var connection = amqp.createConnection({
+var connection = null ;
+var queue = null;
+
+function start(){
+    var conn = amqp.createConnection({
     host: '52.37.50.140'
     , port: 5672
     , login: 'alozano'
     , password: 'alex506071006'
-});
+},{reconnect: false});
+     conn.on("error", function(err) {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message);
+      }
+    });
+    conn.on("close", function() {
+      console.error("[AMQP] reconnecting");
+        if(queue){
+            queue.destroy();
+        }
+      conn.destroy();
+      return setTimeout(start, 5000);
+    });
+    conn.on('ready',function(){
+        console.log("[AMQP] connected");
+        connection = conn;
+        whenConnected();    
+    });
+    
+}
 
-connection.on('ready', function () {
+function whenConnected(){
     log4c.log('Conexión hecha con RabbitMQ y lista para ser usada');
     dispensador.initDispensador();
-    connection.queue('QEntrada', {
+    queue = connection.queue('QEntrada', {
         durable: true
         , autoDelete: false
     }, function (cola) {
@@ -41,14 +65,12 @@ connection.on('ready', function () {
             }
         });
     });
-});
-
-connection.on('error', function () {
-    console.error(arguments);
-});
+}
 
 function escribirCola(mensaje) {
     connection.publish('QSalida', mensaje);
 }
+
+start();
 
 exports.escribirCola = escribirCola;
